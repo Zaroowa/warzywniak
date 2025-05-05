280736ad39df27937f47251da4de4c58e480255b
Zaroowa <Zarooweczka@wp.pl>
2025-05-05 17:03:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 
 # ---- KONFIGURACJA ----
 GODZINA = 17  # godzina, o której bot ma pingować (24h format)
-MINUTA = 05  # minuta
+MINUTA = 5  # minuta
 CHANNEL_ID = 1303471531560796180  # ID kanału, gdzie ma wysyłać wiadomość
 # -----------------------
 
