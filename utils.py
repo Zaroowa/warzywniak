089e85c14157cc430d6a19624849ba9b27a1d44c
Zaroowa <Zarooweczka@wp.pl>
2026-01-24 18:35:37 +0100

    Add functions for getting time and sending images

--- utils.py
+++ utils.py
@@ -1,1 +1,23 @@
+# utils.py
+import datetime
+import pytz
+import os
+import discord
 
+TZ = pytz.timezone("Europe/Warsaw")
+
+def get_now():
+    return datetime.datetime.now(TZ)
+
+async def send_image(channel, text, image_path=None, mention=None):
+    if image_path and os.path.exists(image_path):
+        await channel.send(
+            text,
+            file=discord.File(image_path),
+            allowed_mentions=discord.AllowedMentions(users=True)
+        )
+    else:
+        await channel.send(
+            text,
+            allowed_mentions=discord.AllowedMentions(users=True)
+        )
