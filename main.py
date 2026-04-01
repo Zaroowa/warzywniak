5ae469ef4223e1587ef28f35ace89637306c8e1b
Zaroowa <Zarooweczka@wp.pl>
2026-04-01 16:11:07 +0200

    Add error handling for command cooldowns

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ keep_alive()
 import os
 import discord
 from discord.ext import commands
+
 from scheduler import scheduler
 from db import connect_db, init_db
 from reactions import handle_reactions
@@ -14,38 +15,50 @@ GUILD_ID = 1292140494314274936
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
     await bot.wait_until_ready()
+
     await connect_db()
     await init_db()
 
     guild = discord.Object(id=GUILD_ID)
     await bot.tree.sync(guild=guild)
-    
+
     if not scheduler.is_running():
         scheduler.start(bot)
 
     print(f"✅ Zalogowano jako {bot.user}")
 
+
 @bot.event
 async def on_message(message):
     if message.author.bot:
         return
+
     await handle_reactions(bot, message)
+
     await bot.process_commands(message)
+
 
 @bot.event
 async def on_command_error(ctx, error):
+
     if isinstance(error, commands.CommandOnCooldown):
+
         minutes = int(error.retry_after // 60)
         seconds = int(error.retry_after % 60)
+
         await ctx.send(
             f"⏳ Spokojnie! `!smaki` będzie dostępne za **{minutes}m {seconds}s**."
         )
+
         return
 
     raise error
 
+
 cmd.setup(bot)
+
 bot.run(os.getenv("TOKEN"))
